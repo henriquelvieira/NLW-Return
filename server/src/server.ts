de09bb7a3001467483f7bedcb70bbdaac12df022
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-05-06 16:46:21 -0300

    Add env PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const API_PORT = process.env.PORT || 3333;
 
 app.use(cors());
 
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, () => { 
-    console.log(`HTTP server is running on port ${PORT}`);
+app.listen(API_PORT, () => { 
+    console.log(`HTTP server is running on port ${API_PORT}`);
 });
