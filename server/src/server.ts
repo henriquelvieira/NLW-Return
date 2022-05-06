e81583fda3f5fad916b4d21397f5b0957152b31c
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-05-06 17:22:55 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(API_PORT, () => { 
+app.listen(process.env.PORT || 3333, () => { 
     console.log(`HTTP server is running on port ${API_PORT}`);
 });
